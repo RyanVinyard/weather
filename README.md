b0961809cfd4f52d69492bc2f310695ec58c5c51
Ryan Vinyard and Thomas Rivas <thomas@>
2016-10-11 16:47:17 -0700

    return humidity and temperature in Kelvin

---
+++ README.md
@@ -0,0 +1,7 @@
+After cloning:
+- bower install
+- npm install
+- npm install browser-sync --save-dev
+- gulp bowerJS to create vendor.min.js
+
+to run: gulp serve
---
+++ js/temperature.js
@@ -0,0 +1,7 @@
+// Weather.prototype.getTemp = function (city, displayFunction) {
+//   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+//     displayFunction(city, response.main.temp);
+//   }).fail(function(error) {
+//     $('.showTemp').text(error.responseJSON.message);
+//   });
+// };
